cb1a87fa1f904df57dbb2c37040e8984c9920de2
Lauren <ldisalvo@bu.edu>
2017-10-24 19:38:03 -0400

    Add files via upload

---
+++ CS411.py
@@ -0,0 +1,41 @@
+import requests
+import flask
+import json
+from flask import Flask, render_template, jsonify, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_form():
+    return render_template('results.html')
+
+
+@app.route('/', methods=['POST', 'GET'])
+def get_query():
+    try:
+        event_type = request.form.get('event_type')
+        print(event_type)
+
+    except:
+        print("no search terms added")
+        return flask.redirect(flask.url_for('get_query'))
+
+    request_link = "https://www.eventbriteapi.com/v3/events/search/?token=IX7T7PME3OGKOEV4Q33E&q=" + event_type
+    response = requests.get(
+        request_link, verify=True,  # Verify SSL certificate
+    )
+    data = response.json()
+
+    event_names = []
+    for i in data['events']:
+        event_names += [str(i['name']['text'])]
+    print(event_names)
+
+    return render_template('eventInfo.html', response=event_names)
+
+
+if __name__ == '__main__':
+    app.run()
+
